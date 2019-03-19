7069f5bb18e98d272db9efe5699482eb57aae551
Irene Saez <bluemilk44@gmail.com>
2019-03-19 15:30:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,15 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name()
+    
+  end
 
 end
